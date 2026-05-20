91468b8e1c10cf84179d9ac9a56ac18eb787aced
星蕊 <xingrui@xingruideMacBook-Air.local>
2026-05-20 17:18:03 +0800

    chore: add .gitignore, scene-data.js, mp3; remove stale large PNGs

---
+++ scene-data.js
@@ -0,0 +1,237 @@
+// Auto-generated sprite data
+window.SPRITE_DATA=[
+  {
+    "name": "标题",
+    "sprites": [
+      {
+        "key": "boy",
+        "src": "boy-stand.png",
+        "x": 184,
+        "y": 171,
+        "h": 58,
+        "flip": true,
+        "anim": {
+          "enabled": false,
+          "dur": 5,
+          "loop": true,
+          "path": [
+            {
+              "x": 175,
+              "y": 190
+            }
+          ]
+        }
+      },
+      {
+        "key": "girl",
+        "src": "girl-stand.png",
+        "x": 213,
+        "y": 171,
+        "h": 58,
+        "flip": false,
+        "anim": {
+          "enabled": false,
+          "dur": 5,
+          "loop": true,
+          "path": [
+            {
+              "x": 210,
+              "y": 190
+            }
+          ]
+        }
+      },
+      {
+        "key": "dog",
+        "src": "dog.png",
+        "x": 239,
+        "y": 204,
+        "h": 22,
+        "flip": false,
+        "anim": {
+          "enabled": false,
+          "dur": 5,
+          "loop": true,
+          "path": [
+            {
+              "x": 250,
+              "y": 200
+            }
+          ]
+        }
+      }
+    ]
+  },
+  {
+    "name": "滑雪",
+    "sprites": [
+      {
+        "key": "boySki",
+        "src": "boy-ski.png",
+        "x": 8,
+        "y": 188,
+        "h": 46,
+        "flip": true,
+        "anim": {
+          "enabled": true,
+          "dur": 7.1,
+          "loop": false,
+          "path": [
+            {
+              "x": -60,
+              "y": 178
+            },
+            {
+              "x": 93,
+              "y": 211
+            },
+            {
+              "x": 205,
+              "y": 227
+            },
+            {
+              "x": 346,
+              "y": 237
+            },
+            {
+              "x": 476,
+              "y": 239
+            }
+          ]
+        }
+      },
+      {
+        "key": "girlSki",
+        "src": "girl-ski.png",
+        "x": 26,
+        "y": 192,
+        "h": 46,
+        "flip": true,
+        "anim": {
+          "enabled": true,
+          "dur": 6.9,
+          "loop": false,
+          "path": [
+            {
+              "x": -30,
+              "y": 178
+            },
+            {
+              "x": 99,
+              "y": 212
+            },
+            {
+              "x": 225,
+              "y": 231
+            },
+            {
+              "x": 364,
+              "y": 239
+            },
+            {
+              "x": 478,
+              "y": 237
+            }
+          ]
+        }
+      },
+      {
+        "key": "dog",
+        "src": "dog.png",
+        "x": 97,
+        "y": 206,
+        "h": 22,
+        "flip": true,
+        "anim": {
+          "enabled": true,
+          "dur": 6,
+          "loop": false,
+          "path": [
+            {
+              "x": 20,
+              "y": 180
+            },
+            {
+              "x": 140,
+              "y": 211
+            },
+            {
+              "x": 282,
+              "y": 229
+            },
+            {
+              "x": 428,
+              "y": 231
+            },
+            {
+              "x": 482,
+              "y": 233
+            }
+          ]
+        }
+      }
+    ]
+  },
+  {
+    "name": "木屋",
+    "sprites": [
+      {
+        "key": "boy",
+        "src": "boy-stand.png",
+        "x": 257,
+        "y": 165,
+        "h": 40,
+        "flip": false,
+        "anim": {
+          "enabled": false,
+          "dur": 5,
+          "loop": true,
+          "path": [
+            {
+              "x": 80,
+              "y": 162
+            }
+          ]
+        }
+      },
+      {
+        "key": "girl",
+        "src": "girl-stand.png",
+        "x": 271,
+        "y": 167,
+        "h": 38,
+        "flip": false,
+        "anim": {
+          "enabled": false,
+          "dur": 5,
+          "loop": true,
+          "path": [
+            {
+              "x": 110,
+              "y": 162
+            }
+          ]
+        }
+      },
+      {
+        "key": "dog",
+        "src": "dog.png",
+        "x": 284,
+        "y": 182,
+        "h": 22,
+        "flip": false,
+        "anim": {
+          "enabled": false,
+          "dur": 5,
+          "loop": true,
+          "path": [
+            {
+              "x": 290,
+              "y": 178
+            }
+          ]
+        }
+      }
+    ]
+  }
+];
